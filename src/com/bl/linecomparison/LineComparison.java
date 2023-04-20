510122d68f34f2d193d8f18f5b68060caf798ced
Kanishk <kanishksinghal06@gmail.com>
2023-04-20 13:18:02 +0530

    Added the Classes and Methods in the Code

--- src/com/bl/linecomparison/LineComparison.java
+++ src/com/bl/linecomparison/LineComparison.java
@@ -1,11 +1,38 @@
 package com.bl.linecomparison;
 import java.util.Scanner;
+class Point {
+    double x;
+    double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+class Line {
+    Point startPoint;
+    Point endPoint;
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+    public double getLength() {
+        double length = Math.sqrt(Math.pow(endPoint.x - startPoint.x, 2) + Math.pow(endPoint.y - startPoint.y, 2));
+        return length;
+    }
+    public boolean isEqual(Line line) {
+        return(this.startPoint.x == line.startPoint.x && this.startPoint.y == line.startPoint.y && this.endPoint.x == line.endPoint.x && this.endPoint.y == line.endPoint.y);
+    }
+    public int compareTo(Line line) {
+        Double length1 = this.getLength();
+        Double length2 = line.getLength();
+        return length1.compareTo(length2);
+    }
+}
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner sc = new Scanner(System.in);
 
-//        UC-1
 
         // Create two lines with different endpoints
         System.out.println("Enter the value of x1: ");
@@ -16,6 +43,10 @@ public class LineComparison {
         double x2 = sc.nextDouble();
         System.out.println("Enter the value of y2: ");
         double y2 = sc.nextDouble();
+        Point startPoint1 = new Point(x1,y1);
+        Point endPoint1 = new Point(x2,y2);
+        Line line1 = new Line(startPoint1,endPoint1);
+
         System.out.println("Enter the value of x3: ");
         double x3 = sc.nextDouble();
         System.out.println("Enter the value of y3: ");
@@ -24,27 +55,21 @@ public class LineComparison {
         double x4 = sc.nextDouble();
         System.out.println("Enter the value of y4: ");
         double y4 = sc.nextDouble();
-        // Calculate the length of each line
-        double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-        // Print the length of each line to the console
-        System.out.println("The length of line1 is " + length1);
-        System.out.println("The length of line2 is " + length2);
-
-//       UC-2
+        Point startPoint2 = new Point(x3,y3);
+        Point endPoint2 = new Point(x4,y4);
+        Line line2 = new Line(startPoint2,endPoint2);
+        System.out.println("The length of Line 1 is "+ line1.getLength() );
+        System.out.println("The length of Line 2 is "+ line2.getLength() );
 
         // Check if the two lines are equal
-        boolean equal = (x1 == x3 && y1 == y3 && x2 == x4 && y2 == y4);
-        if (equal) {
+        if (line1.isEqual(line2)) {
             System.out.println("line1 and line2 are equal");
         } else {
             System.out.println("line1 and line2 are not equal");
         }
 
-//      UC-3
-
         // Compare the two lines using the compareTo method
-        int compareResult = Double.compare(length1, length2);
+        int compareResult = line1.compareTo(line2);
         if (compareResult == 0) {
             System.out.println("line1 and line2 are of equal length");
         } else if (compareResult < 0) {
